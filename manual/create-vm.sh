d942b85b4af295a6246dad6f783c47f80bdd85e4
Murygin Maksim <muriginm@gmail.com>
2017-11-06 13:27:12 +0700

    correct linux service script

--- manual/create-vm.sh
+++ manual/create-vm.sh
@@ -3,9 +3,9 @@
 gcloud compute instances create "vm-manual" \
     --zone "europe-west1-d" \
     --machine-type "n1-standard-1" \
-    --tags "http-server" \
     --image "ubuntu-1604-xenial-v20171011" \
     --image-project "ubuntu-os-cloud" \
+    --tags "http-server" \
     --boot-disk-size "10" \
     --boot-disk-type "pd-ssd" \
     --boot-disk-device-name "vm-manual" \
--- manual/startup.sh
+++ manual/startup.sh
@@ -7,10 +7,6 @@ apt-get install -y build-essential
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
-## Allow inbound access to 80 port without root
-setcap 'cap_net_bind_service=+ep' $(readlink -f $(which node))
-
-
 # Install Redis
 wget http://download.redis.io/redis-stable.tar.gz && \
     tar xvzf redis-stable.tar.gz && \
@@ -21,19 +17,24 @@ wget http://download.redis.io/redis-stable.tar.gz && \
 # Run redis
 redis-server --daemonize yes
 
+# Requirements to run app as non-root
+
+## Add user to run app
+adduser --system --group --no-create-home node-runtime
+## Allow inbound access to 80 port without root
+setcap 'cap_net_bind_service=+ep' $(readlink -f $(which node))
 
 # Install app
-
-## Configure node service
-adduser --system --group --no-create-home node-runtime
-wget https://raw.githubusercontent.com/m-murygin/it-city-2017/master/manual/chat.service -P /etc/systemd/system
-systemctl daemon-reload
 
 ## Clone app
 git clone https://github.com/m-murygin/node-chat.git /opt/chat
 cd /opt/chat
 sudo npm install
 chown -R node-runtime /opt/chat
+
+## Configure node service
+wget https://raw.githubusercontent.com/m-murygin/it-city-2017/master/manual/chat.service -P /etc/systemd/system
+systemctl daemon-reload
 
 ## Run app
 systemctl start chat
